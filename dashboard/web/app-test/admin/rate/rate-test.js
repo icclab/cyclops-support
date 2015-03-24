edc5af2bfc990da447c5d4a854bc8e1ae9a92d90
Beni Truninger <zoopajr@gmail.com>
2015-03-24 17:51:18 +0100

    template for static rate

--- dashboard/web/app-test/admin/rate/rate-test.js
+++ dashboard/web/app-test/admin/rate/rate-test.js
@@ -7,7 +7,10 @@ describe('controller', function() {
     /*
         Fake Data
      */
-
+    var disabledClass = "disabled";
+    var enabledClass = "";
+    var statusStringOn = "on";
+    var statusStringOff = "off";
 
     /*
         Test setup
@@ -44,9 +47,33 @@ describe('controller', function() {
     /*
         Tests
      */
-    describe('method', function() {
-        it('should do', function() {
+    describe('setStaticRateEnabled', function() {
+        it('should set staticRateStatusString to "on" if enabled', function() {
+            controller.staticRateStatusString = statusStringOff;
+            controller.setStaticRateEnabled(true);
+            expect(controller.staticRateStatusString).toEqual(statusStringOn);
+        });
 
+        it('should set staticRateStatusString to "off" if disabled', function() {
+            controller.staticRateStatusString = statusStringOn;
+            controller.setStaticRateEnabled(false);
+            expect(controller.staticRateStatusString).toEqual(statusStringOff);
+        });
+
+        it('should set button classes correctly if enabled', function() {
+            controller.enabledButtonClass = enabledClass;
+            controller.disabledButtonClass = disabledClass;
+            controller.setStaticRateEnabled(true);
+            expect(controller.enabledButtonClass).toEqual(disabledClass);
+            expect(controller.disabledButtonClass).toEqual(enabledClass);
+        });
+
+        it('should set button classes correctly if disabled', function() {
+            controller.enabledButtonClass = disabledClass;
+            controller.disabledButtonClass = enabledClass;
+            controller.setStaticRateEnabled(false);
+            expect(controller.enabledButtonClass).toEqual(enabledClass);
+            expect(controller.disabledButtonClass).toEqual(disabledClass);
         });
     });
 });
--- dashboard/web/app/admin/rate/rate.js
+++ dashboard/web/app/admin/rate/rate.js
@@ -11,6 +11,18 @@
     AdminRateController.$inject = ['$log', 'restService'];
     function AdminRateController($log, restService) {
         var me = this;
+        this.isStaticRateEnabled = true;
+        this.staticRateStatusString = "on";
+        this.enabledButtonClass = "disabled";
+        this.disabledButtonClass = "";
+        this.meters = [];
+
+        this.setStaticRateEnabled = function(isEnabled) {
+            me.isStaticRateEnabled = isEnabled;
+            me.staticRateStatusString = isEnabled ? "on" : "off";
+            me.enabledButtonClass = isEnabled ? "disabled" : "";
+            me.disabledButtonClass = isEnabled ? "" : "disabled";
+        };
     }
 
 })();
